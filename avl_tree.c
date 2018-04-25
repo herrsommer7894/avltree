33a221a260959c9eb55c2c39b92e25932c856600
chaosheng <chaosheng@sohu-inc.com>
2018-04-25 19:05:21 +0800

    完善avl的插入与删除，未测试！

--- avl_tree.c
+++ avl_tree.c
@@ -24,13 +24,16 @@ static inline int avl_tree_get_height(avl_tree_node_t *node, avl_tree_node_t *se
 
 static inline avl_tree_node_t* avl_tree_back_track_recompute_children_height(avl_tree_node_t *root, avl_tree_node_t *current, avl_tree_node_t *sentinel);
 
-static inline 
+static inline avl_tree_node_t* avl_tree_middle_travel_get_post_node(avl_tree_node_t *start, avl_tree_node_t *sentinel);
+
+static inline avl_tree_node_t* avl_tree_find_node(avl_tree_node_t *root, int key, avl_tree_node_t *sentinel);
+
 /*init a avl-tree root*/
-void
+int
 avl_tree_init(avl_tree_t *tree, avl_tree_node_t *sentinel, avl_tree_node_insert_func i_func)
 {
 	if(NULL == tree){
-		return;
+		return -1;
 	}
 	avl_tree_init_sentinel(sentinel);
 	tree->sentinel = sentinel;
@@ -38,12 +41,43 @@ avl_tree_init(avl_tree_t *tree, avl_tree_node_t *sentinel, avl_tree_node_insert_
 	tree->root = sentinel;
 }
 
-/*insert a node into avl-tree */
-void
-avl_tree_insert(avl_tree_t *tree, avl_tree_node_t *node)
+/* insert a key into avl-tree */
+int 
+avl_tree_insert_key(avl_tree_t *tree, int key)
 {
 	if(NULL == tree){
-		return ;
+		return -1;
+	}
+	avl_tree_node_t *node = (avl_tree_node_t *)calloc(1,sizeof(avl_tree_node_t));
+	if(NULL == node){
+		return  -2;
+	}
+	node->key = key;
+	node->left_child = node->right_child = tree->sentinel;
+	node->height = 1;
+	return avl_tree_insert_node(tree, node);
+}
+
+/* delete key from avl-tree */
+int 
+avl_tree_delete_key(avl_tree_t *tree, int key)
+{
+	if(NULL == tree){
+		return -1;
+	}
+	avl_tree_node_t * target = avl_tree_find_node(tree->root, key, tree->sentinel);
+	if(target == tree->sentinel){
+		return -3;
+	}
+	return avl_tree_delete_node(tree, target);
+}
+
+/*insert a node into avl-tree */
+int
+avl_tree_insert_node(avl_tree_t *tree, avl_tree_node_t *node)
+{
+	if(NULL == tree){
+		return -1;
 	}
 	avl_tree_node_t *sentinel, *temp, *root, *first_bf;
 
@@ -56,7 +90,7 @@ avl_tree_insert(avl_tree_t *tree, avl_tree_node_t *node)
 		node->right_child = sentinel;
 		node->height = 1;
 		root = node;
-		return;
+		return 0;
 	}
 	/* insert a node using BST feature */
 	first_bf = tree->insert_func(tree, node);
@@ -86,13 +120,81 @@ avl_tree_insert(avl_tree_t *tree, avl_tree_node_t *node)
 		/* via back-track algorithm to recompute children' height and get other unbalanced node */
 		first_bf = avl_tree_back_track_recompute_children_height(root, node, sentinel);
 	}
+
+	return 0;
 }
 
 /*delete a node from avl-tree */
-void
-avl_tree_delete(avl_tree_t *tree, avl_tree_node_t *node)
+int
+avl_tree_delete_node(avl_tree_t *tree, avl_tree_node_t *node)
 {
+	avl_tree_node_t *root, *sentinel, *temp, *first_bf;
+	if(NULL == tree || NULL == node){
+		return -1;
+	}
 
+	root = tree->root;
+	sentinel = tree->sentinel;
+
+	/* firstly deletion and recompute height , re-balance if necessary */
+	if(node->left_child == sentinel && node->right_child == sentinel){	/* the would-be deleted node is leaf node */
+		if(node == node->parent->left_child){
+			node->parent->left_child = sentinel;
+		}else{
+			node->parent->right_child = sentinel;
+		}
+	}else if(node->left_child != sentinel && node->right_child == sentinel){	/* the would-be deleted node only have left child node */
+		if(node->parent->left_child == node){
+			node->parent->left_child = node->left_child;
+		}else{
+			node->parent->right_child = node->left_child;
+		}
+		node->left_child->parent = node->parent;
+	}else if(node->right_child != sentinel && node->left_child == sentinel){	/* the would-be deleted node only have right child node */
+		if(node->parent->left_child == node){
+			node->parent->left_child = node->right_child;
+		}else{
+			node->parent->right_child = node->right_child;
+		}
+		node->right_child->parent = node->parent;
+	}else{	/* the would-be deleted node both have two nodes */
+		temp = avl_tree_middle_travel_get_post_node(node->right_child, sentinel);
+		/* exchange key */
+		node->key = temp->key;
+		/* delete temp */
+		temp->parent->left_child = temp->left_child;
+		temp->parent = NULL;
+	}
+	/* via back-track algorithm to recompute children' height and get other unbalanced node */
+	first_bf = avl_tree_back_track_recompute_children_height(root, node->parent, sentinel);
+		
+	/* re-balance avl-tree if necessary */	
+	while(first_bf != sentinel){
+		if(avl_tree_get_height(first_bf->left_height, sentinel) > avl_tree_get_height(first_bf->right_height, sentinel)){//L
+			temp = first_bf->left_child;
+			if(avl_tree_get_height(temp->left_height, sentinel) > avl_tree_get_height(temp->right_height, sentinel)){//L
+				/* LL type, just a step : right rotate */
+				avl_tree_right_rotate(&root, first_bf, sentinel);
+			}else{//R
+				/* LR type, two step : firstly left ratate and right rotate */
+				avl_tree_left_rotate(&root,first_bf->right_child,sentinel)
+				avl_tree_right_rotate(&root, first_bf, sentinel);
+			}
+		}else{//R
+			if(avl_tree_get_height(temp->left_height, sentinel) > avl_tree_get_height(temp->right_height, sentinel)){//L
+				/* RL type, two step: firstly right rotate and left rotate */
+				avl_tree_right_rotate(&root, first_bf->left_child, sentinel);
+				avl_tree_left_rotate(&root,first_bf,sentinel)
+			}else{//R
+				/* RR type, just a  step: left rotate */
+				avl_tree_left_rotate(&root, first_bf, sentinel);
+			}
+		}
+		/* via back-track algorithm to recompute children' height and get other unbalanced node */
+		first_bf = avl_tree_back_track_recompute_children_height(root, node, sentinel);
+	}
+
+	return 0;
 }
 
 /* insert a node into tree in BST way */
@@ -100,17 +202,15 @@ avl_tree_node_t*
 bst_insert_node(avl_tree_t *tree, avl_tree_node_t *node)
 {
 	avl_tree_node_t *temp = tree->root, *parent = NULL;
-	for(; temp != tree->sentinel; ){
+	for( ; temp && temp != tree->sentinel; ){
 		parent = temp;
 		temp = temp->key > node->key? temp->left_child: temp->right_child;
 	}
 	 node->parent = parent;
 	 temp = node;
-	 node->left_child = node->right_child = tree->sentinel;
-	 node->height = 1;
 
 	/* via back-track algorithm to recompute children' height and get other unbalanced node */
-	return avl_tree_back_track_recompute_children_height(tree->root, node, tree->sentinel);
+	return avl_tree_back_track_recompute_children_height(tree->root, node->parent, tree->sentinel);
 }
 
 /* left rotate */
@@ -193,14 +293,38 @@ static inline avl_tree_node_t*
 avl_tree_back_track_recompute_children_height(avl_tree_node_t *root, avl_tree_node_t *current, avl_tree_node_t *sentinel)
 {
 	av_tree_node_t* first_bf = sentinel;
-	int left_height = 0, right_height = 0;
-	for(; current->parent != NULL ; ){
-		left_height = 0,right_height = 0;
+	for(; current && current->parent != NULL ; ){
 		current->height = avl_tree_get_height(current, sentinel);
 		if(abs(avl_tree_get_height(current->left_child,sentinel) - avl_tree_get_height(current->right_child,sentinel)) > 1 && first_bf == sentinel){
 			first_bf = current;
+			break;
 		}
 		current = current->parent;
 	}
 	return first_bf;
+}
+
+static inline avl_tree_node_t*
+avl_tree_middle_travel_get_post_node(avl_tree_node_t *start, avl_tree_node_t *sentinel)
+{
+	avl_tree_node_t *temp = start;
+	for( ; temp && temp->left_child != sentinel ; ){
+		temp = temp->left_child;
+	}
+	return temp;
+}
+
+static inline avl_tree_node_t*
+vl_tree_find_node(avl_tree_node_t *root, int key, avl_tree_node_t *sentinel)
+{
+	avl_tree_node_t *temp = root, ret = sentinel;
+
+	for( ; temp && temp != sentinel; ){
+		if(temp->key == key ){
+			ret = temp;
+			break;
+		}
+		temp = temp->key > key ?temp->left_child: temp->right_child;
+	}
+	return ret;
 }
--- avl_tree.h
+++ avl_tree.h
@@ -23,13 +23,27 @@ struct avl_tree_s{
 	avl_tree_node_insert_func insert_func;
 };
 
-void avl_tree_init(avl_tree_t *tree, avl_tree_node_t *sentinel, avl_tree_node_insert_func i_func);
+/*  
+	the type of returns is int type , 0 is OK ,others are failed 
+   	fthe type of returns is point type, NULL is failed,others are OK
+*/
 
-void avl_tree_insert(avl_tree_t *tree, avl_tree_node_t *node);// insert a node into avl-tree
+/* init a avl-tree */
+int avl_tree_init(avl_tree_t *tree, avl_tree_node_t *sentinel, avl_tree_node_insert_func i_func);
 
-void avl_tree_delete(avl_tree_t *tree, avl_tree_node_t *node);//delete a node from avl-tree
+/* insert a key into avl-tree */
+int avl_tree_insert_key(avl_tree_t *tree, int key);
 
-avl_tree_node_t* bst_insert_node(avl_tree_t *tree, avl_tree_node_t *node);//insert a node into tree via BST way
+/* insert a node into avl-tree */
+int avl_tree_insert_node(avl_tree_t *tree, avl_tree_node_t *node);
 
+/* delete key from avl-tree */
+int avl_tree_delete_key(avl_tree_t *tree, int key);
+
+/* delete a node from avl-tree */
+int avl_tree_delete_node(avl_tree_t *tree, avl_tree_node_t *node);
+
+/* insert a node into tree via BST way */
+avl_tree_node_t* bst_insert_node(avl_tree_t *tree, avl_tree_node_t *node);
 
 #endif /* _AVL_TREE_H_ */
